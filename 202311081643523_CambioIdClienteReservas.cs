de7c3e14876b1dba4153681683c6414d3d6bccba
GuillermoReus <149092190+GuillermoReus@users.noreply.github.com>
2023-11-12 16:56:57 +0100

    Add files via upload

---
+++ 202311081643523_CambioIdClienteReservas.cs
@@ -0,0 +1,24 @@
+﻿namespace NetWork.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CambioIdClienteReservas : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Reservas", "IdCliente", c => c.Int(nullable: false));
+            CreateIndex("dbo.Reservas", "IdCliente");
+            AddForeignKey("dbo.Reservas", "IdCliente", "dbo.Clientes", "IdCliente", cascadeDelete: true);
+            DropColumn("dbo.Reservas", "DniCliente");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Reservas", "DniCliente", c => c.String());
+            DropForeignKey("dbo.Reservas", "IdCliente", "dbo.Clientes");
+            DropIndex("dbo.Reservas", new[] { "IdCliente" });
+            DropColumn("dbo.Reservas", "IdCliente");
+        }
+    }
+}
---
+++ Reservas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetWork.Modelo
+{
+    public class Reservas
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CodigoReservas {  get; set; }
+        public DateTime FechaEntrada { get; set; }
+        public DateTime FechaSalida{ get; set; }
+        public int IdCliente { get; set; }
+        public int NumHabitacion { get; set; }
+        public int EstadoReserva {  get; set; }
+        [NotMapped]
+        public string EstadoReservaTexto
+        {
+            get
+            {
+                return EstadoReserva == 1 ? "Firmado" : "No Firmado";
+            }
+        }
+        [NotMapped]
+        public string NombreCliente { get; set; }
+        [NotMapped]
+        public int TelefonoCliente { get; set; }
+
+        public Clientes Cliente { get; set; }
+
+        public Reservas(int codigoReservas, DateTime fechaEntrada, DateTime fechaSalida, int idCliente, int numHabitacion, int estadoReserva, string nombreCliente)
+        {
+            CodigoReservas = codigoReservas;
+            FechaEntrada = fechaEntrada;
+            FechaSalida = fechaSalida;
+            IdCliente = idCliente;
+            NumHabitacion = numHabitacion;
+            EstadoReserva = estadoReserva;
+            NombreCliente = nombreCliente; 
+        }
+        public Reservas() { }
+
+    }
+}
