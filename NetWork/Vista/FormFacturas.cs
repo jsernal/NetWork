d57ff31ed1dc72ba1ae05fa43eadc253bc5d9a1c
Jorge Olleros <jolleros@uoc.edu>
2023-11-13 15:45:38 +0100

    nuevo update modificacionReservas y facturas + añadir comentarios en código.

--- NetWork/Vista/FormFacturas.cs
+++ NetWork/Vista/FormFacturas.cs
@@ -1,12 +1,8 @@
 ﻿using NetWork.Modelo;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Linq;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace NetWork.Vista
 {
@@ -21,28 +17,6 @@ namespace NetWork.Vista
 
         ConexionDB db;
         Facturas factura = new Facturas();
-
-       /* public void Create(Facturas factura)
-        {
-            {
-                try
-
-                {
-                    using (db = new ConexionDB())
-                    {
-                        db.Facturas.Add(factura);
-                        db.SaveChanges();
-                    }
-
-                }
-
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-
-            }
-        }*/
 
         public List<Facturas> Read()
         {
@@ -62,28 +36,7 @@ namespace NetWork.Vista
 
         }
 
-        public void Update(Facturas factura)        
-        {
-            {
-                try
-
-                {
-                    using (db = new ConexionDB())
-                    {
-                        db.Entry(factura).State = EntityState.Modified;
-                        db.SaveChanges();
-                    }
-
-                }
-
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-
-            }
-        }
-        public void Delete(int numFactura)
+     /*   public void Delete(int numFactura)
         {
             {
                 try
@@ -103,8 +56,9 @@ namespace NetWork.Vista
                 }
 
             }
-        }
+        }*/
 
+        //Buscar servicios por NIF
         public List<Facturas> buscarPorNif(string dniCliente)
         {
             try
@@ -126,13 +80,8 @@ namespace NetWork.Vista
 
         private void cargarDatos()
            {
-          //     factura.NumFactura = Convert.ToInt32(textboxNumFactura.Text);
                factura.IdCliente = textBoxNifCliente.Text;
-            // factura.CodigoServicio = Convert.ToInt32(textBoxCodServicio.Text);
-               factura.TotalFactura = Convert.ToDecimal(textTotal.Text);
-
-
-            cargarGrid();
+                cargarGrid();
            }
 
         private void cargarGrid()
@@ -145,33 +94,10 @@ namespace NetWork.Vista
         {
             cargarGrid();
         }
-        private void limpiarDatos()
-        {
-            //textboxNumFactura.Focus();
-            //textboxNumFactura.Text = string.Empty;
-            textBoxNifCliente.Text = string.Empty;
-            //dateTimePickerFecha.Text = string.Empty;
-            //textBoxCodServicio.Text = string.Empty;
-            cargarGrid();
-
-        }
-
-
-
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
-
-     /*   private void btnEliminar_Click(object sender, EventArgs e)
-        {
-            {
-
-                cargarDatos();
-                Delete(factura.NumFactura);
-                limpiarDatos();
-            }
-        } */
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -181,27 +107,7 @@ namespace NetWork.Vista
 
             this.Close();
         }
-
-        private void textBoxTotalFactura_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
+        //Mostrar en Grid solo aquellos servicios para un DNI concreto
         private void textBoxNifCliente_TextChanged(object sender, EventArgs e)
         {
             if (textBoxNifCliente.Text != String.Empty)
@@ -218,24 +124,25 @@ namespace NetWork.Vista
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        //Mostrar cantidad total factura
+        private void button2_Click(object sender, EventArgs e)
         {
+            if (textBoxNifCliente.Text == string.Empty)
+            {
+                textTotal.Text = string.Empty;
+            }
+            double suma = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[1].Value.Equals(textBoxNifCliente.Text))
+                {
+                    suma += Convert.ToDouble(dataGridView1.Rows[i].Cells["TotalFactura"].Value.ToString());
+                }
+            }
             
+            textTotal.Text = suma.ToString();
+
         }
 
-        private void textTotal_TextChanged(object sender, EventArgs e)
-        {
-        }
-    }
+     }
  }
-
-        /*   private void btnGuardarCambios_Click(object sender, EventArgs e)
-           {
-               if (textboxCodigoReserva.Text != string.Empty)
-               {
-                   cargarDatos();
-                   Update(factura);
-                   limpiarDatos();
-               }
-
-           }*/
