a76d44bb8ec163f390044f4944f6d712b21e4067
rmontesauoc <102413280+rmontesauoc@users.noreply.github.com>
2023-11-14 15:23:23 +0100

    Create FormLogin.cs

---
+++ NetWork/Vista/FormLogin.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace HotelSOL
+{
+    public partial class FormLogin : Form
+    {
+        private const string ConexionString = "Data Source=(local);Initial Catalog=DB;Integrated Security=True";
+        
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string nombre = txtUsuario.Text;
+            string contraseña = txtContraseña.Text;
+
+            using (ConexionDB db = new ConexionDB())
+            {
+                conexion.Open();
+
+                string query = "SELECT COUNT(*) FROM Usuarios WHERE Nombre=@Nombre AND Contraseña=@Contraseña";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Nombre", nombre);
+                    command.Parameters.AddWithValue("@Contraseña", contraseña);
+
+                    int count = (int)command.ExecuteScalar();
+
+                    if (count > 0)
+                    {
+                        MessageBox.Show("Inicio de sesión exitoso");
+                      
+                    }
+                    else
+                    {
+                        MessageBox.Show("Inicio de sesión fallido. Nombre de usuario o contraseña incorrectos");
+                    }
+                }
+            }
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            FormRegistro formRegistro = new FormRegistro();
+            formRegistro.ShowDialog();
+        }
+    }
+}
