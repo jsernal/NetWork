2bcb46b36dfd75c3b6b2c63bffeaa7c8b4fc38fd
serx3 <serx31994@hotmail.com>
2023-11-26 12:55:24 +0100

    no va

--- NetWork/Controlador/OdooClientes.py
+++ NetWork/Controlador/OdooClientes.py
@@ -2,8 +2,8 @@ import xml.etree.ElementTree as ET
 import xmlrpc.client
 
 url = 'http://localhost:8069'
-DB='dataguru'
-USER='khlazovska@uoc.edu'
+DB='Network'
+USER='sbrudi@uoc.edu'
 PASS='password'
 
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
@@ -14,7 +14,7 @@ models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 uid = common.authenticate(DB, USER, PASS, {})
 
 if uid:
-    archivo_xml = ET.parse('C:/Users/kater/OneDrive/Documentos/NET_PROD4/DataGuru/ProyectoGuruData/Controlador/Clientes.xml')
+    archivo_xml = ET.parse('C:/Users/sergi/Desktop/GIT/Network/NetWork/Controlador/Clientes.xml')
     xml = archivo_xml.getroot()
     for i in xml:
         do_write = models.execute(DB,uid,PASS,'x_clientes','create',[{
